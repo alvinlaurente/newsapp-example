5767e44458c84a8a8e0b1cf83c4ee3df40af0516
Alvin Laurent Ertanto <alvin.ertanto@gmail.com>
2024-01-11 03:22:25 +0700

    Navbar topic

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,14 +2,48 @@ import { Link } from 'react-router-dom'
 import { Search } from '../components'
 
 const Navbar = () => {
+	const renderTopic = () => {
+		const topicsComponent = []
+		const topics = [
+			'Indonesia',
+			'World',
+			'Politic',
+			'Business',
+			'Economy',
+			'Health',
+			'Sport',
+			'Technology',
+			'Travel',
+			'Entertainment',
+			'Culinary',
+			'Game'
+		]
+
+		topics.map((topic) => {
+			const url = `/search?q=${topic}&limit=12&page=1`
+			const url2 = `/topic/${topic}`
+			topicsComponent.push(
+				<Link key={topic} to={url2}>
+					<div className="w-full h-12 flex flex-row justify-items-center items-center hover:text-[#7404FA]">
+						<p className="font-bold">{topic}</p>
+					</div>
+				</Link>
+			)
+		})
+
+		return topicsComponent
+	}
   return (
 		<div className='bg-[#a790a7] py-2'>
 			<div className='flex flex-row items-center justify-between container mx-auto'>
 				<Link to="/">
-					<div className="px-4 w-full h-12 flex flex-row justify-items-center items-center">
+					<div className="w-full h-12 flex flex-row justify-items-center items-center hover:text-[#7404FA]">
 						<p className="font-bold text-xl">React News</p>
 					</div>
 				</Link>
+				<div className='flex flex-row gap-8'>
+					{ renderTopic() }
+				</div>
 				<Search />
 			</div>
 		</div>
